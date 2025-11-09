bb432f71048a3ce3737f8688c6f22363f4dd7883
lelooouch <118593450+lelooouch@users.noreply.github.com>
2025-11-09 13:39:04 +0500

    Update urls.py

--- starwars/peple/urls.py
+++ starwars/peple/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('luke_skywalker/', views.get_luke_info),
     path('X-wing/', views.get_X_wing),
-    #path('next-starship/', views.get_next_starship)
+    
 ]
 
